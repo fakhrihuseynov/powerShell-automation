dc9c00375bcba8a123ad06f3b28955a6906f0f4a
Fakhri Huseynov <fexrihuseynov@gmail.com>
2022-03-30 14:18:10 +0400

    Adding full property names for conclusion

--- module3-eventlog-HTML/systemReport-Standard.ps1
+++ module3-eventlog-HTML/systemReport-Standard.ps1
@@ -2,7 +2,7 @@
 Get a breakdown of error source in the System event log
 #>
 $Computername = $env:COMPUTERNAME
-$data = Get-EventLog System -EntryType Error -Newest 1000 | Group Source -NoElement -Computer
+$data = Get-EventLog System -EntryType Error -Newest 1000 | Group-Object Source -NoElement -Computer
 $title = "System Log Analysis"
 $css = "./sample.css"
-$data | Sort Count, Name -Descending | Select Count, Name | ConvertTo-Html -Title $title -Pre "<H1>$Computername<H1>" -CssUri $css | Out-File c:\work\systemsources.html
+$data | Sort-Object Count, Name -Descending | Select-Object Count, Name | ConvertTo-Html -Title $title -Pre "<H1>$Computername<H1>" -CssUri $css | Out-File c:\work\systemsources.html
