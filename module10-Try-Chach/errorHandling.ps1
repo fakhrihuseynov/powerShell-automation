8e4c7b0cbaaf2349f4788ea7cca5f76443a7e16f
Fakhri Huseynov <fexrihuseynov@gmail.com>
2022-04-01 17:01:36 +0400

    adding errorHandling script

---
+++ module10-Try-Chach/errorHandling.ps1
@@ -0,0 +1,10 @@
+# Try to run this command
+# You must force errors to be 'terminating'
+
+$computerName = "foo"
+try {
+    Get-Service BITS -ComputerName $computerName -ErrorAction Stop
+}
+catch {  # <--- Here is catching the errors 
+    Write-Warning "Failed to find what are you looking for on host ---->: $computerName.$($_exeception.message)"
+}
